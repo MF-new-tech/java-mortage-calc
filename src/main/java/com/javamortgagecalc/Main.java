fe5993bf55d7466631471ec7ea4bbf29839c10f0
Mike Flynn <mdflynn34@outlook.com>
2021-06-28 12:52:54 -0600

    add principle entry

--- src/main/java/com/javamortgagecalc/Main.java
+++ src/main/java/com/javamortgagecalc/Main.java
@@ -1,8 +1,16 @@
 package com.javamortgagecalc;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public final class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        double principal = scanner.nextDouble();
+        NumberFormat principalCurrency = NumberFormat.getCurrencyInstance();
+        String principalResult = principalCurrency.format(principal);
+        System.out.println("Principal:  " + principalResult);
     }
 }
