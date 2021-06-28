aff639114e58645ab789281eafd11276c42c0871
Mike Flynn <mdflynn34@outlook.com>
2021-06-28 13:54:29 -0600

    add apr and period

--- src/main/java/com/javamortgagecalc/Main.java
+++ src/main/java/com/javamortgagecalc/Main.java
@@ -6,11 +6,24 @@ import java.util.Scanner;
 public final class Main {
 
     public static void main(String[] args) {
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+        // Get user input in command line
         Scanner scanner = new Scanner(System.in);
+
+        // Get user Principal amount
         System.out.print("Principal: ");
         double principal = scanner.nextDouble();
-        NumberFormat principalCurrency = NumberFormat.getCurrencyInstance();
-        String principalResult = principalCurrency.format(principal);
-        System.out.println("Principal:  " + principalResult);
+
+        // Get user interest rate
+        System.out.print("Annual Interest Rate: ");
+        float apr = scanner.nextFloat();
+        float monthlyApr = apr / PERCENT / MONTHS_IN_YEAR;
+
+        // Get user loan period
+        System.out.print("Period (Years): ");
+        byte loanYears = scanner.nextByte();
+        int numberOfPayments = loanYears * MONTHS_IN_YEAR;
+
     }
 }
